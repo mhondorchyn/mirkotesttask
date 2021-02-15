eb873c286e6a0cb4e3dd69ac11df6e9df0d85d4e
mhondorchyn <hondorchynmirko@gmail.com>
2021-02-15 21:36:50 +0200

    second attempt to land a job

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
-const $ = require('jquery');
+$(document).ready(function(){
+    $('.js-main-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: false,
+        asNavFor: '.js-main-slider-nav'
+      });
+      $('.js-main-slider-nav').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        asNavFor: '.js-main-slider',
+        arrows: false,
+        focusOnSelect: true,
+        mobileFirst: true,
+        responsive: [
+            {
+              breakpoint: 767,
+              settings: {
+                vertical: true
+              }
+            },
+          ]
+      });
+  });
 
-$(document).ready(() => {
-    const button = $('.js-button');
-    button.on('click', (e) => alert('Hello'));
-});
+
+
